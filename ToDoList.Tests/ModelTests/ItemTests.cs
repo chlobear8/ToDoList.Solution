e38bdfb0e209fecbd08994e42d981a2bf511905a
Chloe ONeil <chloesporri@yahoo.com>
2023-02-01 20:07:45 -0800

    pass first test to confirm item can be taken in

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -6,6 +6,11 @@ namespace ToDoList.TestTools
   [TestClass]
   public class ItemTests
   {
-    
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item();
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
   }
 }
