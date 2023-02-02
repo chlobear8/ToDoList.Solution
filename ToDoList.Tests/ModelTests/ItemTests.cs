f8c28d5cc0c39ff1468ac11022309632acd1df4a
Chloe ONeil <chloesporri@yahoo.com>
2023-02-01 20:18:23 -0800

    pass second test taking in description

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -9,8 +9,21 @@ namespace ToDoList.TestTools
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
-      Item newItem = new Item();
+      Item newItem = new Item("test");
       Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+    [TestMethod]
+      public void GetDescription_ReturnsDescription_String()
+      {
+        //Arrange (gather, declare & create)
+        string description = "Walk the dog.";
+        Item newItem = new Item(description);
+
+        //Act (invoke the functionality call method or retrieving property)
+        string result = newItem.Description;
+
+        //Assert (confirm the functionality works as anticipated)
+        Assert.AreEqual(description, result);
     }
   }
 }
