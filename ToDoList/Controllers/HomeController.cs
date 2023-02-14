46d044d81b528d26bf538297ef4d5f73debe6523
Chloe ONeil <chloesporri@yahoo.com>
2023-02-14 14:42:11 -0800

    add two routes for /items

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -5,12 +5,25 @@ namespace ToDoList.Controllers
 {
   public class HomeController : Controller
   {
-    
+
     [Route("/")]
     public ActionResult Index()
     {
       Item starterItem = new Item("Add first item to To Do List");
       return View(starterItem);
+    }
+
+    [Route("/items/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    [Route("/items")]
+    public ActionResult Create(string description)
+    {
+      Item myItem = new Item(description);
+      return View("Index", myItem);
     }
   }
 }
