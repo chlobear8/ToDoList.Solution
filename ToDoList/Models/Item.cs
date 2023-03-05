67184edacb41a8a51a5e1b40ce834c9e28eb8208
Chloe ONeil <chloesporri@yahoo.com>
2023-03-05 13:01:39 -0800

    add get and set methods to Item.cs for Categories

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -9,5 +9,7 @@ namespace ToDoList.Models
   {
     public string Description { get; set; }
     public int ItemId { get; set; }
+    public int CategoryId { get; set;}
+    public Category category { get; set; }
   }
 }
