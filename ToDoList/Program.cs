feff4f8533d72aeb84ce46ac5346665bc80704a7
Chloe ONeil <chloesporri@yahoo.com>
2023-03-26 10:26:30 -0700

    add LoginView

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -42,5 +42,37 @@ using Microsoft.AspNetCore.Identity;
 
       app.Run();
 
+      builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+          .AddEntityFrameworkStores<ToDoListContext>()
+          .AddDefaultTokenProviders();
+
+    //to do this for testing/overriding default requirements
+
+// builder.Services.Configure<IdentityOptions>(options =>
+// {
+//   // Default Password settings.
+//   options.Password.RequireDigit = true;
+//   options.Password.RequireLowercase = true;
+//   options.Password.RequireNonAlphanumeric = true;
+//   options.Password.RequireUppercase = true;
+//   options.Password.RequiredLength = 6;
+//   options.Password.RequiredUniqueChars = 1;
+// });
+
+// WebApplication app = builder.Build();
+
+
+    //and how to change them
+
+// builder.Services.Configure<IdentityOptions>(options =>
+// {
+//   options.Password.RequireDigit = false;
+//   options.Password.RequireLowercase = false;
+//   options.Password.RequireNonAlphanumeric = false;
+//   options.Password.RequireUppercase = false;
+//   options.Password.RequiredLength = 0;
+//   options.Password.RequiredUniqueChars = 0;
+// });
+
 
     
