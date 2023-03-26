8f3c48862f9bb83b6d8c15e9e8c895dcf3a1c223
Chloe ONeil <chloesporri@yahoo.com>
2023-03-26 09:37:12 -0700

    add boiler to Application

--- ToDoList/Models/ApplicationUser.cs
+++ ToDoList/Models/ApplicationUser.cs
@@ -5,6 +5,8 @@ namespace ToDoList.Models
 {
     public class ApplicationUser : IdentityUser
     {
-
+      public string Website { get; set; }
+      public string Image { get; set; }
+      public DateOnly Birthday { get; set; }
     }
 }
